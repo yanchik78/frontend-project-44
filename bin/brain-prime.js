2360a66fdd26f06641e8319457b70822657b8bdc
Gelmen <danil.danich.22825@gmail.com>
2025-03-04 13:19:29 +0300

    9

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,1 +1,43 @@
 "brain-prime"`: "bin/brain-prime.js"`
+
+
+import readlineSync from 'readline-sync'
+
+
+const primenum = (num) => {
+  if (num < 2) return false
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false
+  }
+  return true
+}
+
+const primeGame = () => {
+  console.log('Welcome to the Brain Games!')
+  const userName = readlineSync.question('May I have your name? ')
+  console.log(Hello, ${userName}!)
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".')
+
+  const rounds = 3 
+  let correctAnswers = 0
+
+  for (let i = 0; i < rounds; i++) {
+    const number = Math.floor(Math.random() * 100)
+    console.log(Question``: ${number})
+    const userAnswer = readlineSync.question('Your answer: ').toLowerCase()
+
+    const correctAnswer = primenum(number) ? 'yes' : 'no'
+
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!')
+      correctAnswers += 1
+    } else {
+      console.log('${userAnswer}' `is wrong answer; (Correct answer was '${correctAnswer}').)
+    }
+  }
+
+  console.log(Congratulations, ${userName}!);
+  console.log(You answered correctly ${correctAnswers} out of ${rounds} questions.)
+}
+
+primeGame()
